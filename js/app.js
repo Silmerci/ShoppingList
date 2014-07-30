f368c0a60d4a6a52b838932b9037db22dff8e69a
Silmerci <silcoding@gmail.com>
2014-07-29 22:38:27 -0400

    Jquert Not working

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	function getItem() {
+		$('.add').keydown(function (enter) {
+			console.log("typed character in list")
+			if (enter.keyCode == 13) {
+				postItem();
+			}
+		});
+	}
+
+	getItem();
+
+})
+
