5f6d7fb8ced68f2c451daec032c25f1df14723e7
Silmerci <silcoding@gmail.com>
2014-08-12 20:12:18 -0400

    Jquery Updates

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
-$(document).ready(function() {
-	function getItem() {
-		$('.add').keydown(function (enter) {
-			console.log("typed character in list")
-			if (enter.keyCode == 13) {
-				postItem();
-			}
-		})
+$(document).ready(function(){
+	$('#item').focus();
+
+	function getItem(){
+		return $("#item").val();
 	}
 
-	getItem();
+	$("#add").on("click",function(e){
+		e.preventDefault();
+		e.stopPropagation();
 
-})
+		if (getItem() === '') {
+			alert("Hmmm... item please");
+			$('#item').focus();
 
+		} else {
+			var newItem = '<li><input type="checkbox" class="checkbox"><span>' + getItem() + '</span><button type="reset" value="Delete" class="delete"></button</li>';
+			$(newItem).appendTo("#itemList");
+
+			$("#item").val('');
+			$('#item').focus();
+		}
+	});
+
+	$("#itemList").on("click", ".checkbox", function(){
+		$(this).siblings('span').toggleClass('lineThrough').show();
+	});
+
+	$("#itemList").on("click", ".delete", function() {
+		$(this).closest('li').remove();
+	});
+
+});
